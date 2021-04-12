10262c7bb4a077277aef08d9d608ec684f1998fa
kotala-sudo <pkotala@yahoo.com>
2021-04-11 21:21:01 -0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, VERSION } from "@angular/core";
+import { map, filter, take } from "rxjs/operators";
+import { interval, Observable, pipe } from "rxjs";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  constructor() {}
+
+  ngOnInit() {
+    interval(1000)
+      .pipe(
+        take(5),
+        map(v => Date.now())
+      )
+      .subscribe(value => console.log("Subscriber: " + value));
+  }
+}
