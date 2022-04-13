7d23036fd23bb9fb0f0143b3663618850e4dbe5a
kotala-sudo <pkotala@yahoo.com>
2022-04-12 21:41:11 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,22 @@
-import { Component } from "@angular/core";
-import { map, filter, take, reduce } from "rxjs/operators";
-import { interval, pipe } from "rxjs";
+import { Component } from '@angular/core';
+import { map, filter, take, reduce } from 'rxjs/operators';
+import { interval, pipe } from 'rxjs';
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   constructor() {}
-
+  //check the app for notes on these rxjs operators and make sure to open the console on developer tools
   ngOnInit() {
     interval(1000)
       .pipe(
-        take(3),
-        filter(x => x%2 == 0),
-        map(v => Date.now())
+        take(5),
+        filter((x) => x % 2 == 0),
+        map((v) => Date.now())
       )
-      .subscribe(value => console.log("Subscriber: " + value));
-
-     }
-
-
+      .subscribe((value) => console.log('Subscriber: ' + value));
+  }
 }
